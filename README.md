66f11a256ed44a2b7528fb0f71f62b32337776b7
Aleksandar Seovic <aleks@seovic.com>
2020-01-15 00:27:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Helidon Sock Shop 
+
---
+++ update.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+function inv {
+    "$@"
+    local status=$?
+    if [ ${status} -ne 0 ]; then
+        exit $?
+    fi
+    return ${status}
+}
+
+function handle_trap {
+    echo "Terminating script..."
+    exit 0
+}
+
+trap 'handle_trap' SIGINT
+
+USE_HTTP=false
+
+while getopts "hrstda:u:l" opt;
+do
+    case ${opt} in
+        h)
+            USE_HTTP=true
+            ;;
+        s)
+            DO_STATUS=true
+            ;;
+        t)
+            DO_STASH=true
+            ;;
+        d)
+            DO_DIFF=true
+            ;;
+        a)  AFTER=$OPTARG
+            ;;
+        u)  UNTIL=$OPTARG
+            ;;
+        l)  DO_LOG=true
+            ;;
+        r)  UP_REMOTE=true
+            ;;
+        : )
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+            ;;
+        \?) echo "Usage: update.sh [-h (use HTTPS to checkout)] [-s do git status instead of pull --rebase] [-t do git stash list instead of pull --rebase] [-a after -u until -l log]  [-d do git diff instead of pull --rebase]"
+            [ $PS1 ] && return || exit;
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+PROJECTS=(
+    "sockshop"
+    "carts"
+    "catalog"
+    "orders"
+    "payment"
+    "shipping"
+    "users"
+)
+BASE=$PWD
+
+for i in "${PROJECTS[@]}"
+do
+    if [ ${UP_REMOTE} ]; then
+        if [ ${USE_HTTP} ]; then
+            git remote set-url origin https://github.com/helidon-sock-shop/${i}.git
+        else
+            git remote set-url origin git@github.com:helidon-sock-shop/${i}.git
+        fi
+        continue
+    fi
+    if [ ! -d ${i} ]; then
+        if [ ${USE_HTTP} = true ]; then
+            inv git clone https://github.com/helidon-sock-shop/${i}.git
+        else
+            inv git clone git@github.com:helidon-sock-shop/${i}.git
+        fi
+    fi
+    if [ ${DO_DIFF} ]; then
+        cd ${i}; echo "***${i}***"
+        inv git --no-pager diff
+        cd ${BASE}
+    elif [ ${DO_STATUS} ]; then
+        cd ${i}; echo "***${i}***"
+        inv git status
+        cd ${BASE}
+    elif [ ${DO_STASH} ]; then
+        cd ${i}; echo "***${i}***"
+        inv git --no-pager stash list
+        cd ${BASE}
+    elif [ ${DO_LOG} ]; then
+        cd ${i}; echo "***${i}***"
+        inv git --no-pager log --after="${AFTER}" --until="${UNTIL}"
+        cd ${BASE}
+    else
+        cd ${i}
+        echo "Pulling latest changes for ${i}"
+        inv git pull --rebase
+        cd ${BASE}
+    fi
+done
